060913552a3691d4d12efd4e6c21bf12c0a2c2bd
anneschjoedt <anne.schjoedt@gmail.com>
2021-04-07 13:39:31 +0200

    emilys ændringer

--- src/stamen.js
+++ src/stamen.js
@@ -21,6 +21,7 @@ var dataMessages = [
   "<ul> <li>Javascript set variable</li><li>Sne i april sang Prince</li><li>Allan Randrup Thomsen</li><li>Hvor mange må man være sammen?</li><li> Indefrosne feriepenge</li><li> Varme hveder hvorfor</li><li>Corona test brain scrape</li><li>How does airdrop work?</li><li>Lovechild 1979 sale</li></ul>",
   "<ul> <li>Paw patrol data</li><li>Ugly helmet</li><li>Ankarsrum pizza</li><li>Dag svanæs</li><li>Her velo</li><li>Bumble</li><li>Anastasia beverly hills brow gel</li><li>How does Brexit affect Mexico?</li><li>Little women</li><li>Oysters and grill</li></ul>",
   "<ul> <li>Cute map of copenhagen</li><li>Is there a spell to become a mermaid that actually works</li><li>Bearnaise</li><li>Funny noises</li><li>How many days in march?</li><li>Cheap gifts for your girlfriend</li><li>How can I be a Pokemon go coach?</li><li>Tine Lindhardt</li><li>Dengue</li><li>Furry slippers</li></ul>",
+  "<ul> <li>Github</li><li>Chicken step</li><li>Convert svg to png</li><li>Carhartt dungarees womens straight</li><li>Dont touch remote control</li><li>Becoming a supple leopard</li><li>How to cut your own hair</li><li>Kiropraktik i centrum</li><li>Kaggle tinder dataset</li><li>Vltj</li></ul>",
 ];
 
 // make objects from the class
@@ -140,6 +141,27 @@ circles[3] = L.circle([55.68356, 12.53664], {
   //fillOpacity: 0.5,
   radius: 200,
 }).addTo(map);
+// add a cicrcle to place 4 in the array
+circles[4] = L.circle([55.67055, 12.54999], {
+  color: "transparent",
+  fillColor: "transparent",
+  //fillOpacity: 0.5,
+  radius: 200,
+}).addTo(map);
+// add a cicrcle to place 5 in the array
+circles[5] = L.circle([55.69921, 12.58042], {
+  color: "transparent",
+  fillColor: "transparent",
+  //fillOpacity: 0.5,
+  radius: 200,
+}).addTo(map);
+// add a cicrcle to place 6 in the array
+circles[6] = L.circle([55.05143, 10.60156], {
+  color: "transparent",
+  fillColor: "transparent",
+  //fillOpacity: 0.5,
+  radius: 200,
+}).addTo(map);
 
 function openPopup(e) {
   var popup = L.popup()
@@ -177,7 +199,7 @@ Chicken.on("drag", function (e) {
         autoPan: true,
       })
         .addTo(map)
-        .bindPopup(dataMessages[getRandomInt(0, 3)]);
+        .bindPopup(dataMessages[getRandomInt(0, 4)]);
     }
   });
 });
